b76d4994c5eff0873bcfc26535bcf371dfcdcffd
Luan Eduardo <reissouza123@gmail.com>
2017-02-24 11:49:48 -0300

    Revert "Removed #include of Function.h"
    This reverts commit 0a9a9c71a8fe0443f38491ddc9b37451b4d4f149.

--- LearnCpp2.10_Question3/LearnCpp2.10_Question3/Functions.cpp
+++ LearnCpp2.10_Question3/LearnCpp2.10_Question3/Functions.cpp
@@ -1,5 +1,6 @@
 #include "stdafx.h"
 #include <iostream>
+#include "Functions.h"
 
 /* Limpa erros e caracteres no buffer de entrada.
 */
@@ -22,19 +23,6 @@ double getUserDoubleNumber()
 	limparBuffer();
 
 	return number;
-}
-
-/* Verifica se o operador inserido e valido.
-*/
-bool checkOperator(char check_operator)
-{
-	if ((check_operator == '+') || (check_operator == '-') || (check_operator == '*') || (check_operator == '/'))
-	{
-		return true;
-	}
-	// Qualquer caracter diferente de +, -, * e / e invalido.
-	std::cout << "\nOperador invalido!\n";
-	return false;
 }
 
 /* Obtem um operador matematico do usuario com um Char,
@@ -60,6 +48,19 @@ char getUserOperator()
 		// FALSE -> faz uma chamada recursiva ate que o operador seja valido.
 		getUserOperator();
 	}
+}
+
+/* Verifica se o operador inserido e valido.
+*/
+bool checkOperator(char check_operator)
+{
+	if ((check_operator == '+') || (check_operator == '-') || (check_operator == '*') || (check_operator == '/'))
+	{
+		return true;
+	}
+	// Qualquer caracter diferente de +, -, * e / e invalido.
+	std::cout << "\nOperador invalido!\n";
+	return false;
 }
 
 /* Faz o calculo dos Double's conforme o operador informado;
