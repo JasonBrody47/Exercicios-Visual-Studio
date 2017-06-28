39d68cee83b221613b4e7bfd541e04fc9f11f10d
Luan Eduardo <reissouza123@gmail.com>
2017-06-28 13:40:58 -0300

    mersenne-twister: Fisrt upload

---
+++ mersenne-twister/mersenne-twister/mersenne-twister.cpp
@@ -0,0 +1,27 @@
+// Cógido de exemplo, ainda não sei aplicar nada disto.
+
+#include "stdafx.h"
+#include <iostream>
+#include <random> // Para std::random_device e std::mt19937
+// #include <ctime> // Descomente se usando Code::Blocks
+
+int main()
+{
+    // Use uma fonte de entropia de hardware se disponível, caso contrário use PRNG.
+    std::random_device rd;
+    // Inicializa o Mercenne Twister com uma seed aleatória.
+    std::mt19937 mersenne(rd());
+
+    // Imprime vários números aleatórios.
+    for (int count = 0; count < 48; ++count)
+    {
+        std::cout << mersenne() << "\t";
+
+        // Se imprimir 4 números, comece uma nova linha.
+        if ((count + 1) % 4 == 0)
+            std::cout << "\n";
+    }
+
+    std::cin.get();
+    return 0;
+}
