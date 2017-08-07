58a813ae097d5e87c959630a0eac33a44b087cc2
Luan Eduardo <reissouza123@gmail.com>
2017-08-07 17:09:58 -0300

    First Upload

---
+++ array-fixo-test/array-fixo-test/array-fixo-test.cpp
@@ -0,0 +1,30 @@
+// Teste com matriz (array) de tamanho fixo.
+
+#include "stdafx.h"
+#include <iostream>
+
+namespace animals {
+    enum Animals
+    {
+        CHICKEN,
+        DOG,
+        CAT,
+        ELEPHANT,
+        DUCK,
+        SNAKE,
+        MAX_ANIMALS
+    };
+}
+
+int main()
+{
+    double temperature[365]{ 0.0 };
+    std::cout << "Temperatura no dia 30: " << temperature[29] << "\n";
+
+    int animals[animals::MAX_ANIMALS]{2, 4, 4, 4, 2, 0};
+    std::cout << "Um elefante tem " << animals[animals::ELEPHANT] << " patas\n";
+
+    std::cin.get();
+    return 0;
+}
+
