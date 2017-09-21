2de648e7d8bf669edfa4c4d6ff50378bfb3ec379
Luan Eduardo <reissouza123@gmail.com>
2017-09-21 15:21:38 -0300

    pointers-test: First upload

---
+++ pointers-test/pointers-test/pointerstest.cpp
@@ -0,0 +1,29 @@
+// Testes basicos com ponteiros.
+
+#include "stdafx.h"
+#include <iostream>
+#include <typeinfo>
+
+int main()
+{
+    int x{ 9 };
+    std::cout << "Valor da variavel 'x': " << x << "\n";
+    std::cout << "Endereco da variavel 'x': " << &x << "\n";
+    std::cout << "Valor contido no endereco de memoria da variavel 'x': " << *&x << "\n";
+
+    int *pointer{ &x };
+    std::cout << "Endereco armazenado em 'pointer': " << pointer << "\n";
+
+    std::cout << "\nTipo de ponteiro retornado por &(address-of) da variavel 'x': " << 
+        typeid(&x).name() << "\n\n";
+
+    std::cout << "Valor da variavel('x') que esta sendo apontada por 'pointer': " <<
+        *pointer << "\n";
+
+    std::cout << "Tamanho do ponteiro 'pointer' : " << sizeof(pointer) << "\n";
+
+    std::cin.get();
+
+    return 0;
+}
+
