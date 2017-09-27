15fb918ca8616a9f50bda9c9a1ad2a8428439538
Luan Eduardo <reissouza123@gmail.com>
2017-09-27 16:41:56 -0300

    for-each-loop: first upload

---
+++ for-each-loop/for-each-loop/foreachloop.cpp
@@ -0,0 +1,31 @@
+
+#include "stdafx.h"
+#include <iostream>
+#include <string>
+
+int main()
+{
+    std::string names[]{ "Alex", "Betty", "Caroline", "Dave", "Emily", "Fred", "Greg", "Holly" };
+
+    std::cout << "Insira um nome > ";
+    std::string user_name;
+    std::getline(std::cin, user_name);
+
+    bool wasFound{ false };
+    for (const auto &name : names)
+    {
+        if (name == user_name)
+        {
+            wasFound = true;
+            break;
+        }
+    }
+    if (wasFound)
+        std::cout << user_name << " foi encontrado no array.\n";
+    else
+        std::cout << user_name << " nao foi encontrado no array.\n";
+
+    std::cin.get();
+    return 0;
+}
+
