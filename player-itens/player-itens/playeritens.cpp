fc80cc0e9c6fdb590d8ba33043629bf9a41257d2
Luan Eduardo <reissouza123@gmail.com>
2017-09-30 16:24:56 -0300

    player-itens: first upload

---
+++ player-itens/player-itens/playeritens.cpp
@@ -0,0 +1,39 @@
+// playeritens.cpp: Define o ponto de entrada para a aplicação de console.
+//
+
+#include "stdafx.h"
+#include <cstdint>
+#include <iostream>
+
+namespace Itens
+{
+    enum Itens
+    {
+        HEALTH_POTIONS,
+        TORCHES,
+        ARROWS,
+        TOTAL_ITENS
+    };
+}
+
+int16_t countTotalIntems(const int16_t items_amount[]);
+
+int main()
+{
+    int16_t items_amount[Itens::TOTAL_ITENS]{ 2, 5, 10 };
+
+    std::cout << "Total de itens do player: " << countTotalIntems(items_amount);
+
+    std::cin.get();
+    return 0;
+}
+
+int16_t countTotalIntems(const int16_t items_amount[])
+{
+    int32_t totalItems{ 0 };
+    for (int16_t count = 0; count < Itens::TOTAL_ITENS; ++count)
+        totalItems += items_amount[count];
+
+    return totalItems;
+}
+
