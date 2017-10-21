4502c4dfc6413fd1f115727174e40ecdc12ef173
Luan Eduardo <reissouza123@gmail.com>
2017-10-21 14:23:46 -0200

    basic-calculator: fisrt upload

---
+++ basic-calculator/basic-calculator/basic-calculator.h
@@ -0,0 +1,33 @@
+#pragma once
+#ifndef BASIC_CALCULATOR
+#define BASIC_CALCULATOR
+
+#include <cstdint>
+
+using arithmeticFcn = int32_t(*)(int32_t, int32_t);
+
+struct arithmeticStruct
+{
+    char math_operator;
+    arithmeticFcn fcn;
+};
+
+int32_t getUserInteger();
+inline bool isValidInteger();
+char getUserOperator();
+inline bool isValidOperator(char user_operator);
+inline int32_t add(int32_t number_1, int32_t number_2);
+inline int32_t subtract(int32_t number_1, int32_t number_2);
+inline int32_t multiply(int32_t number_1, int32_t number_2);
+inline int32_t divide(int32_t number_1, int32_t number_2);
+arithmeticFcn getArithmeticFunction(char user_operator);
+
+static arithmeticStruct arithmeticArray[]
+{
+    { '+', add },
+    { '-', subtract },
+    { '*', multiply },
+    { '/', divide }
+};
+
+#endif // !BASIC_CALCULATOR
---
+++ basic-calculator/basic-calculator/main.cpp
@@ -0,0 +1,19 @@
+
+#include "stdafx.h"
+#include "basic-calculator.h"
+#include <iostream>
+
+int main()
+{
+    int32_t number_1{ getUserInteger() };
+    char user_operator{ getUserOperator() };
+    int32_t number_2{ getUserInteger() };
+
+    arithmeticFcn arithmeticFunction{ getArithmeticFunction(user_operator) };
+
+    std::cout << number_1 << " " << user_operator << " " << number_2 <<
+        " = " << arithmeticFunction(number_1, number_2) << "\n";
+
+    std::cin.get();
+    return 0;
+}
